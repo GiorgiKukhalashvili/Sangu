5f4c7e73aed404859ea6e9f96ccf4f5018cd7d3e
GiorgiKukhalashvili <g.kukhalashvili23@sangu.edu.ge>
2025-03-31 22:02:19 +0400

    logger commit

---
+++ logger.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Logger {
+    public static void log(string[] args) {
+        Console.WriteLine("Text Input: ");
+        string text = Console.ReadLine() ?? "0";
+
+        while(string.IsNullOrEmpty(text)) {
+            Console.WriteLine("Text Input: ");
+            text = Console.ReadLine() ?? "0";
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Text Output: {text}");
+    }
+}
